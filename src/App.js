de2d19ed8faa7217fd032d433d7730cba321bfd5
nayzawaung750nza <nayzawaung750@gmail.com>
2024-02-01 21:46:53 +0630

    undo code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,57 @@
-import React, { useState } from 'react'
+import logo from './logo.svg';
+import './App.css';
+import image from './assets/recent-project-1.png';
+import { useState } from 'react';
+function App() {
 
-export default function App() {
-  let [count, setCount] = useState(0);
-  let increment = () =>{
-    setCount((prevState) => prevState + 1);
-    setCount((prevState) => prevState + 1);
-    setCount((prevState) => prevState + 1);
-    setCount((prevState) => prevState + 1);
+  let [name, setName] = useState("Nay Zaw Aung");
+  let [posts, setPosts] = useState([
+    {
+      id:1, 
+      title:'First Post',
+    },
+    {
+      id:2, 
+      title:'Second Post',
+    },
+    {
+      id:3, 
+      title:'Third Post',
+    }
+  ]);
+  let changeName = () =>{
+    setName('Aung Aung');
   }
+
   return (
-    <div>
-      <h1>Counter</h1>
-      <h3>Count - {count}</h3>
-      <button onClick={increment}>increase</button>
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+
+        {/* absolute path */}
+      {/* <img src="http://localhost:3000/recent-project-1.png" /> */}
+
+      {/* relative path */}
+      {/* <img src="/recent-project-1.png" /> */}
+
+      {/* 2 nd way */}
+
+      {/* <img src={image} /> */}
+
+        <p>
+          Hello {name}
+        </p>
+        <button onClick={changeName}>Change Name</button>
+
+        <ul>
+          {posts.map((post)=>(
+            <li key={post.id}>{post.title}</li>
+          ))}
+        </ul>
+      </header>
+ 
     </div>
-  )
+  );
 }
+
+export default App;
