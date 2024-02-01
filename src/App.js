71c76741c08f343bbb7f9ae1f9bb549de46a22db
nayzawaung750nza <nayzawaung750@gmail.com>
2024-02-01 14:48:34 +0630

    two-way-of-using-images-in-react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import image from './assets/recent-project-1.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+
+        {/* absolute path */}
+      {/* <img src="http://localhost:3000/recent-project-1.png" /> */}
+
+      {/* relative path */}
+      {/* <img src="/recent-project-1.png" /> */}
+
+      {/* 2 nd way */}
+
+      <img src={image} />
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
+ 
     </div>
   );
 }
