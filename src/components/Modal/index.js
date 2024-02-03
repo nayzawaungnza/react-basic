08539e692b78827b5efa9b66d09f79be42224169
nayzawaung750nza <nayzawaung750@gmail.com>
2024-02-04 00:26:20 +0630

    ep-24 dynamic classname and module css

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -2,14 +2,12 @@ import React from 'react'
 import './index.css'
 import  ReactDOM  from 'react-dom'
 export default function Modal({children, danger=false}) {
+  let className = danger ? 'border-red' : 'border-blue';
   return (
     ReactDOM.createPortal(
     <div className='modal-components'>
         <div className='modal-backdrop'>
-      <div className='modal' style={{ 
-        border: '2px solid',
-        borderColor : danger ? 'red' : 'blue',
-       }}>
+      <div className={`modal ${className}`}>
           {children}
       </div>
     </div>
--- src/components/PostsList/index.js
+++ src/components/PostsList/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './index.css'
+import styles from './single_post.module.css'
 export default function PostsList({posts}) {
   return (
     <div className='post-list'>
         {posts.map(post => (
-            <div className='single-post' key={post.id}>{post.title}</div>
+            <div className={`single-post ${styles.card}`} key={post.id}>{post.title}</div>
         ))}
         
       </div>
