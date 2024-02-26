bf6b2e1ef847f6e61f9069c00b9847e31bf3cafc
nayzawaung750nza <nayzawaung750@gmail.com>
2024-02-26 14:33:51 +0630

    Ep-26-React-2-way-data-binding

--- src/components/PostForm/index.js
+++ src/components/PostForm/index.js
@@ -5,14 +5,19 @@ export default function PostForm() {
   // let onChangeHandler = (e) =>{
   //   setTitle(e.target.value);
   // }
+  let resetForm = () =>{
+    setTitle('');
+    console.log('update successfully');
+  }
   return (
     <div className='post-form-components'>
       <h3>Create Post</h3>
         <form className='post-form'>
         <div className='form-group'>
             <label >Title</label>
-            <input type="text" onChange={(e)=>{setTitle(e.target.value)}} className='form-control' />
+            <input type="text" onChange={(e)=>{setTitle(e.target.value)}} className='form-control' value={title} />
             <p>{title}</p>
+            <button type='button' onClick={resetForm}>Reset Form</button>
         </div>
         <div className='form-group'>
             <button className='form-control'>Post Now</button>
